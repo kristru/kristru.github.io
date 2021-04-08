48162eddd3f7fcafd943dfa693bdafaa064c930a
kristru <krissafuentes@gmail.com>
2021-04-07 20:52:36 -0600

    add portfolio assets and tabs

--- styles/js/main.js
+++ styles/js/main.js
@@ -2,6 +2,24 @@ $(document).ready(function(){
   console.log('testing Jquery library')
 });
 
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
+
+$('#modal1').on('hidden.bs.modal', function (e) {
+  // do something...
+  $('#modal1 iframe').attr("src", $("#modal1 iframe").attr("src"));
+});
+
+$('#modal6').on('hidden.bs.modal', function (e) {
+  // do something...
+  $('#modal6 iframe').attr("src", $("#modal6 iframe").attr("src"));
+});
+
+$('#modal4').on('hidden.bs.modal', function (e) {
+  // do something...
+  $('#modal4 iframe').attr("src", $("#modal4 iframe").attr("src"));
+});
 /* setTimeout(function start (){
 
   $('.bar').each(function(i){
