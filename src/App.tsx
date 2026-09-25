476907a6561a8a61f09eb2641565a818af84d3f0
Kultivate Creative <kultivatecreative@Kultivates-Mac-Studio.local>
2026-09-24 18:36:05 -0600

    simplifying files from vite default and styling homepage layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Header from './components/Header'
 import Home from './pages/Home'
-import './App.css'
+
 
 function App() {
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,45 @@
-import reactLogo from '../assets/react.svg'
+import myPhoto from '../assets/intro-photo.jpg'
+import hello from '../assets/hello,.svg'
+import './Home.css'
+
+const skills = [
+    "UX/UI Designer",
+    "Lifelong Learner",
+    "Problem Solver",
+    "Negotiator",
+    "Project Manager",
+    "Entrepeneur",
+];
+
+const skillColors = [
+    "orange-40",
+    "yellow-40",
+    "blue-40",
+    "purple-30",
+    "pink-40",
+    "green-40",
+];
+
+function getRandomSkillColor(){
+    return skillColors[Math.floor(Math.random() * skillColors.length)];
+}
 
 function Home(){
     return(
         <main className='home-container'>
-            <div className='intro'><span><img src={reactLogo} /></span><span>Hello,</span><span>I'm Krissa</span></div>
-            <p>A strategic designer who bridges the gap between design and engineering.</p>
-            <p>with just the right amount of gumption, I make digital experiences enjoyable.</p>
+            <section className='copy-container'>
+                <div className='intro'><span><img src={myPhoto} width='100'/></span><span><img src={hello} /></span><span className="intro-name body-lg"> I'm Krissa</span></div>
+                <h2>A strategic designer who bridges the gap between design and engineering.</h2>
+                <p>With just the right amount of gumption, I make digital experiences enjoyable.</p>
+            </section>
             
-            <section>
-                <div>UX/UI Designer</div>
-                <div>Lifelong Learner</div>
-                <div>Problem Solver</div>
-                <div>Negotiator</div>
-                <div>Project Manager</div>
-                <div>Entrepreneur</div>
+            <section className='skills'>
+                {skills.map((skill, index) => (
+                    <div className={`skill ${skillColors[index]}`} key={skill}>
+                        {skill}
+                    </div>
+                ))}
+
             </section>
         </main>
     )
